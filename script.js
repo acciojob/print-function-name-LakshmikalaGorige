a6f845d6a7f38c79746d6b14ffdb2b51859cae16
LakshmikalaGorige <124250141+LakshmikalaGorige@users.noreply.github.com>
2023-11-16 13:29:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function functionName() {
+  alert("Function name: " + functionName.name);
+}
+function AccioJob(){
+	alert("AccioJob: " + AccioJob.name)
+}
+AccioJob();
+// Call the function
+functionName();
