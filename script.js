edc60a1d1a06804098e4f39d4073532d19defc41
LakshmikalaGorige <124250141+LakshmikalaGorige@users.noreply.github.com>
2023-11-22 12:25:17 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-//your JS code here. If required.
-function functionName(){
-alert(arguments.callee.name);
-}
- functionName();
- function Acciojob(){
-  alert(arguments.callee.name);
-}
-Acciojob();
